f525982880206069e58447cf8abf705343d27caf
unknown <dtully99@gmail.com>
2024-05-15 08:55:54 -0500

    update init.lua

--- init.lua
+++ init.lua
@@ -126,13 +126,6 @@ require('lazy').setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
       { 'nvim-telescope/telescope-ui-select.nvim' },
       { 'nvim-telescope/telescope-file-browser.nvim' },
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
@@ -145,7 +138,6 @@ require('lazy').setup({
           },
         },
       }
-      pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
       pcall(require('telescope').load_extension 'file_browser')
       local builtin = require 'telescope.builtin'
@@ -265,7 +257,6 @@ require('lazy').setup({
         'stylua',
         'powershell_es',
         'prettier',
-        'markdownlint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
       require('mason-lspconfig').setup {
@@ -319,58 +310,6 @@ require('lazy').setup({
         javascriptreact = { 'prettier' },
       },
     },
-  },
-  { -- Linting
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
-
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
-
-      -- Create autocommand which carries out the actual linting
-      -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          require('lint').try_lint()
-        end,
-      })
-    end,
   },
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -456,30 +395,6 @@ require('lazy').setup({
       end
       require('mini.tabline').setup()
       require('mini.bufremove').setup()
-      require('mini.indentscope').setup { options = { border = 'top', try_as_border = true }, symbol = '│' }
-      vim.api.nvim_create_autocmd({ 'FileType' }, {
-        desc = 'Disable indentscope for certain filetypes',
-        callback = function()
-          local ignore_filetypes = {
-            'aerial',
-            'dashboard',
-            'help',
-            'lazy',
-            'leetcode.nvim',
-            'mason',
-            'neo-tree',
-            'NvimTree',
-            'neogitstatus',
-            'notify',
-            'startify',
-            'toggleterm',
-            'Trouble',
-          }
-          if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-            vim.b.miniindentscope_disable = true
-          end
-        end,
-      })
     end,
   },
   { 'mbbill/undotree' },
