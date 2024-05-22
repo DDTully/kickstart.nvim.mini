e18024276196ac5aa80e0dbc20b007d2ca131a08
unknown <dtully99@gmail.com>
2024-05-22 11:48:42 -0500

    update init.lua

--- init.lua
+++ init.lua
@@ -454,6 +454,18 @@ require('lazy').setup({
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
 
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.powershell = {
+        install_info = {
+          url = 'https://github.com/airbus-cert/tree-sitter-powershell.git',
+          files = { 'src/parser.c' },
+          branch = 'main',
+          generate_requires_npm = false,
+          requires_generate_from_grammar = false,
+        },
+        filetype = 'ps1',
+      }
+
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
       --
